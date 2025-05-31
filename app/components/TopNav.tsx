0afa2fbc7eee7d74cf9f8bcea542345ed254f6f2
Boran Demir <borandemir@Bemirds-Macintosh.local>
2025-06-01 02:06:22 +0300

    Fix navigation and update all latest changes

--- app/components/TopNav.tsx
+++ app/components/TopNav.tsx
@@ -6,10 +6,10 @@ import Link from "next/link";
 export default function TopNav() {
   return (
     <nav className="flex justify-center gap-10 py-4 bg-black/70 backdrop-blur-md text-blue-400 text-lg font-semibold z-50">
-      <Link href="/" className="hover:underline flex items-center gap-1">
+      <Link href="/cv" className="hover:underline flex items-center gap-1">
          <span>About Me</span>
       </Link>
-      <Link href="/book-reviews" className="hover:underline flex items-center gap-1">
+      <Link href="/" className="hover:underline flex items-center gap-1">
          <span>Book Reviews</span>
       </Link>
       <Link href="/contact" className="hover:underline flex items-center gap-1">
